fa407524f6cb84521b96a959431083fc1d44f1bd
fentoncollins123456789 <fentoncollins29@gmail.com>
2024-07-22 11:30:39 +0300

    first commit

--- me.js
+++ me.js
@@ -7,3 +7,33 @@ function closeDrawer() {
     document.getElementById("myDrawer").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+
+  let slideIndex = 0;
+
+  function showSlides(n) {
+    let slides = document.querySelectorAll('.carousel-images img');
+    if (n >= slides.length) {
+      slideIndex = 0;
+    } else if (n < 0) {
+      slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex].style.display = 'block';
+  }
+
+  function moveSlide(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+  }
+
+  
+  showSlides(slideIndex);
+
+  setInterval(function() {
+    moveSlide(1);
+  }, 3000);
